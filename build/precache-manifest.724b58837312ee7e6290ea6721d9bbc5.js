d67a4434d178492a6864224f6ddca1cee6804c11
CommitsVortex <commitsvortex@gmail.com>
2022-01-10 23:14:09 -0600

    build site

---
+++ build/precache-manifest.724b58837312ee7e6290ea6721d9bbc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515f7bd24c69ae135b166354db8226b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79d7cb44594d5c0c1f95",
+    "url": "/static/css/main.07b46f0b.chunk.css"
+  },
+  {
+    "revision": "14c70415477957e9bb03",
+    "url": "/static/js/2.70bc667a.chunk.js"
+  },
+  {
+    "revision": "79d7cb44594d5c0c1f95",
+    "url": "/static/js/main.6e1754bf.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  }
+]);
